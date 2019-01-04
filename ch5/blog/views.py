df7e0ca3bbd3a7dd2c5e7912708ac3681e0d902b
viseth89 <viseth89@gmail.com>
2019-01-04 16:53:51 -0600

    added delete

--- ch5/blog/views.py
+++ ch5/blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -14,3 +16,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name= 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
